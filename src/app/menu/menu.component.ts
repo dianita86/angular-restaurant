1a15e8dd5a9ef8c08d6365d12283c295d58f7540
raomaster <raomaster@gmail.com>
2017-05-02 00:47:04 -0300

    menu apenas y listo

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -6,8 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.less']
 })
 export class MenuComponent implements OnInit {
+ ver: string;
+ producto: string;
+ texto: string;
+ imagen: string;
+ imagen1: string;
+ imagen2: string;
+ imagen3: string;
 
-  constructor() { }
+  constructor() {
+    this.ver = 'Ver más';
+    this.producto = 'producto';
+    this.texto = 'texto';
+    this.imagen = '';
+    this.imagen1 = 'http://www.publicdomainpictures.net/pictures/80000/velka/fresh-tomato-juice.jpg#.WQf3Bv9kPZI.link';
+    this.imagen2 = 'http://www.publicdomainpictures.net/pictures/170000/velka/beer-1462977849MwW.jpg#.WQf6Wa3DrJ4.link';
+    this.imagen3 = 'http://www.publicdomainpictures.net/pictures/160000/velka/orange-drink-at-the-pool-1457022634sVH.jpg#.WQf6hEFSN4I.link';
+  }
+
 
   ngOnInit() {
   }
